26a06df82a1b0f4d92724e33fdc0c45552d4c8f7
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-12-10 17:41:24 +0100

    add router to post new enrolment

--- routes/enrolments.js
+++ routes/enrolments.js
@@ -3,6 +3,31 @@ const router = express.Router();
 const pool = require("../database");
 const Enrolments = require("../models/enrolmentsModel");
 
+//ADD NEW ENROLMENT
+router.post("/", (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: "Request body cannot be empty!",
+    });
+  }
+
+  const enrolment = new Enrolments({
+    EnrolmentID: req.body.EnrolmentID,
+    Mark: 0,
+    CourseID: req.body.CourseID,
+    UserID: req.body.UserID,
+  });
+
+  Enrolments.create(enrolment, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while retrieving enrolments.",
+      });
+    else res.send(data);
+  });
+});
+
 //GET LIST OF AVAILABLE ENROLMENTS
 router.get("/", (req, res) => {
   const Title = req.query.Title;
